2cb8c31f23004a86f6f597c0736310ae26fefc2a
shinri <iwahinome@gmail.com>
2026-07-22 17:58:13 +0700

    begitulah, update v12.2.5 (saweria final)

--- app/src/main/java/ame/project/kanae/overlay/CustomWebOverlaySettingsActivity.kt
+++ app/src/main/java/ame/project/kanae/overlay/CustomWebOverlaySettingsActivity.kt
@@ -1,3 +1,16 @@
+/*
+ * KanaePlayer -
+ * Copyright (C) 2026 KanaePlayer Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; see the
+ * GNU General Public License for more details: <https://www.gnu.org/licenses/>.
+ */
+
 package ame.project.kanae.overlay
 
 import android.content.*
